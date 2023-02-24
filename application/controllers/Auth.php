bd7fb81ab9b05d612980294dd26f95d49061080b
Cahzello <rizkyrasya.m@gmail.com>
2023-02-24 09:41:10 +0700

    regis

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,7 +5,15 @@ class Auth extends CI_Controller {
 
     public function index()
     {
+        $this->load->view('templates/auth_header');
+        $this->load->view('templates/auth_footer');
         $this->load->view('auth/login');
     }
-
+    
+    public function registrasi()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('templates/auth_footer');
+        $this->load->view('auth/registrasi');
+    }
 }
